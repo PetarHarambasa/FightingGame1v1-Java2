a4038f7b49744906d7995436c4a96ad5b5e290e0
PetarHarambasa <croman.3365@gmail.com>
2023-01-05 14:07:55 +0100

    thread for text

---
+++ src/main/java/hr/algebra/java2/thread/ShowTextThread.java
@@ -0,0 +1,35 @@
+package hr.algebra.java2.thread;
+
+import hr.algebra.java2.fightinggame1v1.MainGameScreen;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ShowTextThreadPlayerOne implements Runnable{
+    @Override
+    public void run() {
+        while (true) {
+
+            showText();
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(MainGameScreen.class.getName()).log(
+                        Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public synchronized void showText() {
+        try {
+            System.out.println("Thread is adding a move to a list...");
+            wait();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ShowTextThreadPlayerOne.class.getName()).log(
+                    Level.SEVERE, null, ex);
+        }
+
+        notifyAll();
+    }
+}
